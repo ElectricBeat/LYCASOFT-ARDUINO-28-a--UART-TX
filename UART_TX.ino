252a053f9d09a714d1489184321a36d4d179b450
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-09 07:19:44 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO UART TX AND RX

---
+++ UART_TX.ino
@@ -0,0 +1,25 @@
+
+byte byteRead;
+int i;
+
+void setup() 
+{                
+  Serial.begin(9600);
+  Serial.println("SRI ELECTRONIC AND EMBEDDED SOLUTIONS\n");
+}
+
+void loop() 
+{  
+  Serial.println("SRI ELECTRONIC AND EMBEDDED SOLUTIONS");
+  delay(1000);
+  if (Serial.available())
+  {
+    byteRead= Serial.read();
+   
+    Serial.write(byteRead);
+    if(byteRead == '\n')
+    {
+      Serial.print('\n');
+    }  
+  }
+}
